43941e93db27139bd656df63b14100678838b173
xiaozhu-sorce <790362227@qq.com>
2021-07-14 23:46:48 +0800

    xml

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.calculator;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.githang.statusbar.StatusBarCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button one;
+    private Integer result = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        one = findViewById(R.id.btn_1);
+        one.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result = result+0;
+            };
+        });
+
+        StatusBarCompat.setStatusBarColor(this, 0xFAFAFA,true);
     }
 }
